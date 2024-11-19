d510762c9dba81a950c2e5e4c63bb122d56c0bc9
tikka10 <timo.hyyppa@helsinki.fi>
2024-11-19 20:18:13 +0200

    Update CREATE_TABLES_soundslite_v1.sql
    Conversion of tables creation to sqlite3

--- CREATE_TABLES_soundslite_v1.sql
+++ CREATE_TABLES_soundslite_v1.sql
@@ -1,74 +1,84 @@
-CREATE TABLE IF NOT EXISTS sounds_v1.band
+-- Create band table
+CREATE TABLE IF NOT EXISTS band
 (
-    id serial NOT NULL,
-    bandgearbundle_id integer NOT NULL,
-    bandname varchar(40) NOT NULL,
-    bandimage_path varchar(255),
-    bandsocmedgroupname varchar(40),
-    CONSTRAINT band_pkey PRIMARY KEY (id)
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    bandgearbundle_id INTEGER NOT NULL,
+    bandname VARCHAR(40) NOT NULL,
+    bandimage_path VARCHAR(255),
+    bandsocmedgroupname VARCHAR(40)
 );
+
+-- Create index on bandname
 CREATE INDEX idx_bandname ON band (bandname);
 
-CREATE TABLE IF NOT EXISTS sounds_v1.artist
+-- Create artist table
+CREATE TABLE IF NOT EXISTS artist
 (
-    id serial NOT NULL,
-    artistband_id integer NOT NULL,
-    artistgearbundle_id integer NOT NULL,
-    artistname varchar(40) NOT NULL,
-    artistpassw varchar(80) NOT NULL,
-    artistrole_1 integer NOT NULL,
-    artistrole_2 integer NOT NULL,
-    CONSTRAINT artist_pkey PRIMARY KEY (id)
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    artistband_id INTEGER NOT NULL,
+    artistgearbundle_id INTEGER NOT NULL,
+    artistname VARCHAR(40) NOT NULL,
+    artistpassw VARCHAR(80) NOT NULL,
+    artistrole_1 INTEGER NOT NULL,
+    artistrole_2 INTEGER NOT NULL
 );
+
+-- Create index on artistname
 CREATE INDEX idx_artistname ON artist (artistname);
 
-CREATE TABLE IF NOT EXISTS sounds_v1.gear
+-- Create gear table
+CREATE TABLE IF NOT EXISTS gear
 (
-    id serial NOT NULL,
-    gearbundle_id integer NOT NULL,
-    gearname varchar(50) NOT NULL,
-	gearquantity integer NOT NULL,
-    gearimage_path varchar(255),
-    CONSTRAINT gear_pkey PRIMARY KEY (id)
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    gearbundle_id INTEGER NOT NULL,
+    gearname VARCHAR(50) NOT NULL,
+    gearquantity INTEGER NOT NULL,
+    gearimage_path VARCHAR(255)
 );
+
+-- Create index on gearname
 CREATE INDEX idx_gearname ON gear (gearname);
 
-CREATE TABLE IF NOT EXISTS sounds_v1.gearbundle
+-- Create gearbundle table
+CREATE TABLE IF NOT EXISTS gearbundle
 (
-    id serial NOT NULL,
-    gearbundleband_id integer,
-    gearbundleartist_id integer NOT NULL,
-    gearbundlesound_id integer NOT NULL,
-    gearbundlename varchar(50) NOT NULL,
-    gearbundleimage_path varchar(255),
-    gearbundleuserguide varchar(255) NOT NULL,
-    CONSTRAINT gearbundle_pkey PRIMARY KEY (id)
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    gearbundleband_id INTEGER,
+    gearbundleartist_id INTEGER NOT NULL,
+    gearbundlesound_id INTEGER NOT NULL,
+    gearbundlename VARCHAR(50) NOT NULL,
+    gearbundleimage_path VARCHAR(255),
+    gearbundleuserguide VARCHAR(255) NOT NULL
 );
+
+-- Create index on gearbundlename
 CREATE INDEX idx_gearbundlename ON gearbundle (gearbundlename);
 
-CREATE TABLE IF NOT EXISTS sounds_v1.gearbundlesound
+-- Create gearbundlesound table
+CREATE TABLE IF NOT EXISTS gearbundlesound
 (
-    id serial NOT NULL,
-    gearbundle_id integer NOT NULL,
-    gearbpresetname varchar(50) NOT NULL,
-    gearbpreset_path varchar(255),
-    gearbpsoundclip_path varchar(255),
-    CONSTRAINT gearbundlesound_pkey PRIMARY KEY (id)
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    gearbundle_id INTEGER NOT NULL,
+    gearbpresetname VARCHAR(50) NOT NULL,
+    gearbpreset_path VARCHAR(255),
+    gearbpsoundclip_path VARCHAR(255)
 );
+
+-- Create index on gearbpresetname
 CREATE INDEX idx_gearbpresetname ON gearbundlesound (gearbpresetname);
 
-CREATE TABLE IF NOT EXISTS sounds_v1.artistgearbundle
+-- Create artistgearbundle table
+CREATE TABLE IF NOT EXISTS artistgearbundle
 (
-    id serial NOT NULL,
-    artist_id integer NOT NULL,
-    gearbundle_id integer NOT NULL,
-    CONSTRAINT artistgearbundle_pkey PRIMARY KEY (id)
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    artist_id INTEGER NOT NULL,
+    gearbundle_id INTEGER NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS sounds_v1.bandgearbundle
+-- Create bandgearbundle table
+CREATE TABLE IF NOT EXISTS bandgearbundle
 (
-    id serial NOT NULL,
-    band_id integer NOT NULL,
-    gearbundle_id integer NOT NULL,
-    CONSTRAINT bandgearbundle_pkey PRIMARY KEY (id)
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    band_id INTEGER NOT NULL,
+    gearbundle_id INTEGER NOT NULL
 );
