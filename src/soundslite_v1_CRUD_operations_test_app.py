b8a133bb4d20bb6393ad1dcc95521a8d723c8bb2
tikka10 <timo.hyyppa@helsinki.fi>
2024-11-19 23:27:20 +0200

    Update soundslite_v1_CRUD_operations_test_app.py
    imports added for SQLAlchemy.

--- src/soundslite_v1_CRUD_operations_test_app.py
+++ src/soundslite_v1_CRUD_operations_test_app.py
@@ -1,3 +1,6 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
 
 class DatabaseService:
     # Note:
